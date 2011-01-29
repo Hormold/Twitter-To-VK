d2d4a228e3047e1f04b013c93916e12b3e3cc7bf
Hormold <hormold@gmail.com>
2011-01-29 21:13:06 +0200

    fixed #2

--- twi2vk.php
+++ twi2vk.php
@@ -12,7 +12,7 @@ class twi2vk
 {
 	var $cfg=Array();
 	var $is_debug=1;
-	var $cache_time=1;
+	var $cache_time=10;
 	var $file="last.txt";
 	var $no_rt=0;
 	var $no_rp=1;
@@ -20,18 +20,15 @@ class twi2vk
 	var $time=0;
 	var $debug=Array();
 	//MAIN
-	var $user="";  //TWITTER
-	var $email = ''; //VK LOGIN
-	var $id="";	  //VK ID
-	var $pass = ''; //VK PASS 
 	var $add=" ";//." (via Twitter)";
+	var $no="#nvk";
 
 	function init()
 	{
 		$this->dbg("[init]");
 		$this->fg=file_get_contents($this->file); //Load last message
 		$this->dbg("Loading file: ".$this->file);
-		$this->ft=filemtime($file); //get file time
+		$this->ft=filemtime($this->file); //get file time
 		$this->now=time(); //now time
 		$this->o=$this->now-$this->ft; //diff
 		if($this->o>$this->cache_time){$this->time=1;}
@@ -58,26 +55,35 @@ class twi2vk
 		file_put_contents($this->file,$this->msg);
 		$this->dbg("Save last message to ".$this->file);
 		$this->dbg("NEW STATUS: ".$this->msg);
-		$this->tmp=$a["msg"].$add;
+		$this->tmp=$this->msg.$this->add;
 		$login=$this->login();
 		$this->dbg("Login done...");
-		$this->tmp=$this->curl("http://vkontakte.ru/al_wall.php","act=post&al=1&hash=".$login."&message=".$this->msg."&note_title=&official=&status_export=0&to_id=5385365&type=all");
+		$this->tmp=$this->curl("http://vkontakte.ru/al_wall.php","act=post&al=1&hash=".$login."&message=".$this->msg."&note_title=&official=&status_export=0&to_id=".$this->id."&type=all");
 		$this->dbg("UPDATE done...");
 		$this->dbg("[/vk]");
 	}
 	function twitter()
 	{
 		$this->dbg("[twitter]");
-		$this->tmp = $this->curl("http://api.twitter.com/1/statuses/user_timeline.xml?screen_name=".$this->user,''); 
-		$this->dbg("Loading Twitter API...");
-
+		if(!isset($this->tmp)){
+			$this->tmp = $this->curl("http://api.twitter.com/1/statuses/user_timeline.xml?screen_name=".$this->user,''); 
+			$this->dbg("Loading Twitter API...");
+		}else{
+			$this->dbg("Twitter API already cached...");
+		}
+		//preg_match_all("#<text>(.*)</text>#iU",$this->tmp,$msg); 
 		$this->msg=$this->expl("<text>","</text>",$this->tmp);
 		$this->msg=html_entity_decode($this->msg, ENT_NOQUOTES,'UTF-8');
 		$this->dbg("Twitter: $this->msg");
-
-		$this->rt=$this->expl("<retweeted>","</retweeted>",$this->tmp);
-		//preg_match_all("#post_hash\":\"(.*)\"#iU",$result,$regs); 
+		if(strpos($this->msg,$this->no)){
+			$this->dbg("Stop-word: ".$this->no.". Try next...");
+			$tmp=explode("</text>",$this->tmp);
+			$this->tmp=$tmp[1];
+			$this->twitter();
+		}
 		
+		$this->rt=$this->expl("<retweeted>","</retweeted>",$this->tmp);
+		//
 		$this->rp=$this->expl("<in_reply_to_screen_name>","</in_reply_to_screen_name>",$this->tmp);
 		if($this->no_rt==1 and $this->rt!=="false"){$this->error=1;$this->dbg("ERROR: NO RT!");}
 		if($this->no_rp==1 and $this->rp!==""){$this->error=1;$this->dbg("ERROR: NO @!");}
