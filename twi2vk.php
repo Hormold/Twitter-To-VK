6461122c1989e1fdd52c608ff3ccd8e224572c1f
Hormold <hormold@gmail.com>
2011-02-01 12:35:33 +0200

    SpeedUp

--- twi2vk.php
+++ twi2vk.php
@@ -1,5 +1,5 @@
 <?
-//Beta 0.3
+//Beta 0.4
 $n=new twi2vk;
 $n->init();							  //INIT
 $n->svar("user","");				  //TWITTER LOGIN
@@ -16,7 +16,7 @@ class twi2vk
 	var $twitter_name_to_link=1; //"@test" to "@test (http://twitter.com/test)"
 	var $request_twitter_fullname=1; //"@test" to "Hellow World (http://twitter.com/test)" 
 	var $add=" ";//." (via Twitter)";
-	var $no="#nvk";
+	var $no="#nvk"; //Message with this hashtag not for export/import
 	//END MAIN
 	var $cfg=Array();
 	var $is_debug=1;
@@ -27,7 +27,7 @@ class twi2vk
 	var $error=0;
 	var $time=0;
 	var $debug=Array();
-	var $tmp,$tmp2,$tmp3,$twiname;
+	var $tmp,$tmp2,$tmp3,$twiname,$tid;
 	
 
 	function init()
@@ -75,29 +75,34 @@ class twi2vk
 		$this->tmp=$this->msg.$this->add;
 		$login=$this->login();
 		$this->dbg("Login done...");
-		$this->tmp=$this->curl("http://vkontakte.ru/al_wall.php","act=post&al=1&hash=".$login."&message=".$this->msg."&note_title=&official=&status_export=0&to_id=".$this->id."&type=all");
+		$this->tmp=$this->curl("http://vkontakte.ru/al_wall.php","act=post&al=1&hash=".$login."&message=".urlencode($this->msg)."&note_title=&official=&status_export=0&to_id=".$this->id."&type=all");
 		$this->dbg("UPDATE done...");
 		$this->dbg("[/vk]");
 	}
 
 	function load_twitter()
 	{
+		$this->dbg("[load_twitter]");
 		$this->tmp = $this->curl("http://api.twitter.com/1/statuses/user_timeline.xml?screen_name=".$this->user,''); 
 		$this->dbg("Loading Twitter API...");
 		preg_match_all("#<text>(.*)</text>#iU",$this->tmp,$this->msgs); 
 		preg_match_all("#<in_reply_to_screen_name>(.*)</in_reply_to_screen_name>#iU",$this->tmp,$this->rpl); 
+		if($this->msgs[1][0] and $this->rpl[1][0]){$this->dbg("It's work!");}
 		$this->msgs=$this->msgs[1];
 		$this->rpl=$this->rpl[1];
+		$this->dbg("[/load_twitter]");
 	}
 	
 	function get_twitter_name($name)
 	{
+		$this->dbg("[get_twi_name]");
 		$this->tmp3 = $this->curl("http://api.twitter.com/1/users/show.xml?screen_name=".$name,''); 
-		$this->dbg("Loading Twitter API (2)...");
+		$this->dbg("Loading Twitter API (#2)...");
 		preg_match_all("#<name>(.*)</name>#iU",$this->tmp3,$this->twiname); 
 		$this->twiname=$this->twiname[1][0];
 		$this->twiname=html_entity_decode($this->twiname, ENT_NOQUOTES,'UTF-8');
 		$this->dbg("NAME: ".$this->twiname);
+		$this->dbg("[/get_twi_name]");
 	}
 
 	function twitter()
@@ -124,10 +129,8 @@ class twi2vk
 	function login()
 	{
 		$this->dbg("[login]");
-		$result = $this->curl("http://vkontakte.ru/login.php","email=".$this->email."&pass=".$this->pass); 
 		$this->dbg("Login to VK");
-		$result = $this->curl('http://vkontakte.ru/id'.$this->id,'');
-		$this->dbg("Get hash...");
+		$result = $this->curl("http://vkontakte.ru/login.php","email=".$this->email."&pass=".$this->pass); 
 		preg_match_all("#post_hash\":\"(.*)\"#iU",$result,$regs); 
 		$this->dbg("Parse hash...");
 		$this->dbg("[/login]");
@@ -180,18 +183,19 @@ class twi2vk
 	}
 
 	function linkify_tweet() {
+		$this->dbg("[lt] Start linkify_tweet [/lt]");
 		$this->msg = preg_replace('/(^|\s)@(\w+)/','\1@\2 (http://twitter.com/\2)',$this->msg);
 		$this->msg = preg_replace('/(^|\s)#(\w+)/','\1#\2 (http://search.twitter.com/search?q=%23\2)',$this->msg);
 	}
 
 	function linkify_tweet_2() {
 		if(!$this->tmp2){
-			$this->dbg("[lt2]Start LT2[/lt2]");
+			$this->dbg("[lt2] Start linkify_tweet_2");
 			preg_match_all("/@([A-Za-z0-9_]+)/i",$this->msg,$regs); 
 			$this->tmp2 = $regs[1][0];
-			$this->dbg("[lt2]GET:".$this->tmp2."[/lt2]");
+			$this->dbg("GET:".$this->tmp2." [/lt2]");
 		}else{
-			$this->dbg("[lt2]Start LT2 (2)[/lt2]");
+			$this->dbg("[lt2] Start linkify_tweet_2 (#2) [/lt2]");
 			$this->msg  = str_replace("@".$this->tmp2,$this->twiname." (http://twitter.com/".$this->tmp2.")",$this->msg);
 		}
 		
